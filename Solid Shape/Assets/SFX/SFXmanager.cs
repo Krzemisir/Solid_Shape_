843a052328f2f75d25febee5540ac8728e45b1a5
Krzemisir <38097972+Krzemisir@users.noreply.github.com>
2020-01-23 21:51:55 +0100

    Sound Interface DRAFT

---
+++ Solid Shape/Assets/SFX/SFXmanager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXmanager : MonoBehaviour
+{
+    public AudioSource Fly1;
+    public AudioSource Fly2;
+
+
+    public void PlayFly1()
+    {
+        Fly1.Play();
+    }
+
+    public void PlayFly2()
+    {
+        Fly2.Play();
+    }
+}
