be36df7e8be9914cf50893ea90c90eeb34f8d2b8
Joakim Malmström <malmstrom_9@hotmail.com>
2020-01-24 15:38:06 +0100

    Bite Semi Works

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,30 +5,42 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Transform myTransform;
-    public List<GameObject> flyList;
+    private BoxCollider2D myBoxCollider;
+    private Vector3 playerPosition = new Vector3(0, -2, 0);
 
-    private float playerPostition = 0f;
+    public GameObject flyOne;
+    public GameObject flyTwo;
+    public GameObject flyThree;
+    public GameObject flyFour;
+    public GameObject flyFive;
+
+    private float playerRotationPosition = 0f;
     private float leftRotation = 45f;
     private float rightRotation = -45f;
 
     private bool hasRotationLeft = true;
     private bool hasRotationRight = true;
 
-
-
     void Start()
     {
         myTransform = GetComponent<Transform>();
-        flyList = new List<GameObject>();
+        myBoxCollider = GetComponent<BoxCollider2D>();
+
+        flyOne = new GameObject();
+        flyTwo = new GameObject();
+        flyThree = new GameObject();
+        flyFour = new GameObject();
+        flyFive = new GameObject();
     }
 
     void PlantRotation()
     {
+        
         // CHECKS THE Z ROTATION VALUE
-        myTransform.rotation = Quaternion.Euler(0, 0, playerPostition);
+        myTransform.rotation = Quaternion.Euler(0, 0, playerRotationPosition); 
 
         // CHECKS MAXIMUM ROTATION LEFT
-        if(playerPostition == 90)
+        if (playerRotationPosition == 90)
         {
             hasRotationLeft = false;
         }
@@ -38,7 +50,7 @@ public class Player : MonoBehaviour
         }
 
         // CHECKS MAXIMUM ROTATION RIGHT
-        if (playerPostition == -90)
+        if (playerRotationPosition == -90)
         {
             hasRotationRight = false;
         } 
@@ -52,8 +64,8 @@ public class Player : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                myTransform.rotation = Quaternion.Euler(0, 0, playerPostition + leftRotation);
-                playerPostition = playerPostition + leftRotation;
+                myTransform.rotation = Quaternion.Euler(0, 0, playerRotationPosition + leftRotation);
+                playerRotationPosition = playerRotationPosition + leftRotation;
             }
         }
 
@@ -62,24 +74,52 @@ public class Player : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.RightArrow))
             {
-                myTransform.rotation = Quaternion.Euler(0, 0, playerPostition + rightRotation);
-                playerPostition = playerPostition + rightRotation;
+                myTransform.rotation = Quaternion.Euler(0, 0, playerRotationPosition + rightRotation);
+                playerRotationPosition = playerRotationPosition + rightRotation;
             }
         }
     }
 
     void PlantAttack()
     {
-        if (playerPostition == 90)
+        //float cooldown = 2f;
+        //float timeStamp = Time.time;
+
+        if (Input.GetKeyDown(KeyCode.G))
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            myTransform.position = playerPosition;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (playerRotationPosition == 0)
             {
-                
+                myTransform.position = flyThree.transform.position;
+            }
+
+            if (playerRotationPosition == 45)
+            {
+                myTransform.position = flyTwo.transform.position;
+            }
+
+            if (playerRotationPosition == 90)
+            {
+                myTransform.position = flyOne.transform.position;
+            }
+
+            if (playerRotationPosition == -45)
+            {
+                myTransform.position = flyFour.transform.position;
+            }
+
+            if (playerRotationPosition == -90)
+            {
+                myTransform.position = flyFive.transform.position;
             }
         }
     }
     
-    void Update()
+    void FixedUpdate()
     {
         PlantRotation();
         PlantAttack();
