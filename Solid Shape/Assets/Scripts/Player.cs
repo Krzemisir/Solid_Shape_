12b42dce272930942e71abbc4863c813e9d4a539
Joakim Malmström <malmstrom_9@hotmail.com>
2020-01-24 11:19:05 +0100

    Player Rotation Function

--- Solid Shape/Assets/Scripts/Player.cs
+++ Solid Shape/Assets/Scripts/Player.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Transform myTransform;
+
+    private float playerPostition = 0f;
+    private float leftRotation = 45f;
+    private float rightRotation = -45f;
+
+    private bool hasRotationLeft = true;
+    private bool hasRotationRight = true;
+
     void Start()
     {
-        
+        myTransform = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
+    void PlantRotation()
+    {
+        // CHECKS THE Z ROTATION VALUE
+        myTransform.rotation = Quaternion.Euler(0, 0, playerPostition);
+
+        // CHECKS MAXIMUM ROTATION LEFT
+        if(playerPostition == 90)
+        {
+            hasRotationLeft = false;
+        }
+        else
+        {
+            hasRotationLeft = true;
+        }
+
+        // CHECKS MAXIMUM ROTATION RIGHT
+        if (playerPostition == -90)
+        {
+            hasRotationRight = false;
+        } 
+        else
+        {
+            hasRotationRight = true;
+        }
+        
+        // PLAYER ROTATE LEFT
+        if (hasRotationLeft)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                myTransform.rotation = Quaternion.Euler(0, 0, playerPostition + leftRotation);
+                playerPostition = playerPostition + leftRotation;
+            }
+        }
+
+        // PLAYER ROTATE RIGHT
+        if (hasRotationRight)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                myTransform.rotation = Quaternion.Euler(0, 0, playerPostition + rightRotation);
+                playerPostition = playerPostition + rightRotation;
+            }
+        }
+    }
+    
     void Update()
     {
-        
+        PlantRotation();
     }
 }
