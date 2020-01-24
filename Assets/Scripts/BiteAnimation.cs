d1a1936e0f37d91da1f96d394418c890b9af4ff9
Piglettank <johanfallberg@hotmail.com>
2020-01-24 15:26:17 +0100

    Bite Animation + Fly Sprites

---
+++ Assets/Scripts/BiteAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BiteAnimation : MonoBehaviour
+{
+    Animator plantAnimator;
+
+    public void resetbool()
+    {
+        plantAnimator = gameObject.GetComponent<Animator>();
+        if (plantAnimator.GetBool("isBiting"))
+            {
+                //Stop Animation
+                plantAnimator.SetBool("isBiting", false);
+                Debug.Log("Venus bites!");
+            }
+            else
+            {
+                //Start Animation
+                plantAnimator.SetBool("isBiting", true);
+                Debug.Log("Venus closes mouth...");
+            }
+    }
+}
